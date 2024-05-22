5d3711dd137a51ed6d29efef7966e6c0208069dc
HugoAGvCz <118332303+HugoAGvCz@users.noreply.github.com>
2024-05-21 22:34:52 -0600

    Ya se pueden restablecer las contraseñas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,13 +34,16 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures{
+        viewBinding = true
+    }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.13.0")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-auth:23.0.0")
     implementation("com.google.firebase:firebase-firestore-ktx:25.0.0")
